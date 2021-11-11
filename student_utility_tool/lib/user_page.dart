1decd4f3978ac508e4ec48118eb53441c2d30057
Ting <tingjian.wu@ontariotechu.net>
2021-11-10 22:16:13 -0500

    New Account button can now upload data to the firestore database

--- student_utility_tool/lib/user_page.dart
+++ student_utility_tool/lib/user_page.dart
@@ -1,6 +1,7 @@
 // @dart=2.9
 import 'package:flutter/material.dart';
 import 'streambuilder.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserPage extends StatefulWidget {
   const UserPage({Key key}) : super(key: key);
@@ -15,11 +16,95 @@ class _UserPageState extends State<UserPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(widget.title),
-      ),
-      body: Container(child: StreamBuilderWidget(), padding: EdgeInsets.all(40),)
-    );
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(widget.title),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+                child: Container(
+              child: const StreamBuilderWidget(),
+              padding: const EdgeInsets.all(40),
+            )),
+            Container(
+              child: ElevatedButton(
+                  onPressed: () {
+                    _customDialog();
+                  },
+                  child: const Text("New Account")),
+              padding: const EdgeInsets.only(bottom: 100),
+            )
+          ],
+        ));
+  }
+
+  _customDialog() {
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+    TextEditingController usernameController = TextEditingController();
+    TextEditingController emailController = TextEditingController();
+
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Builder(builder: (BuildContext context) {
+              var height = MediaQuery.of(context).size.height;
+              var width = MediaQuery.of(context).size.width;
+
+              return SizedBox(
+                  height: height - 700,
+                  width: width,
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 30),
+                        TextFormField(
+                          controller: usernameController,
+                          decoration:
+                              const InputDecoration(labelText: "Username"),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'This field cant be empty';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          controller: emailController,
+                          decoration: const InputDecoration(labelText: "Email"),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'This field cant be empty';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+                              if (formKey.currentState.validate()) {
+                                final users = FirebaseFirestore.instance
+                                    .collection('user');
+                                users.add(
+                                    {'username': usernameController.text, 'email': emailController.text});
+                                Navigator.pop(context);
+                              }
+                            },
+                            child: const Text("Create Account"))
+                      ],
+                    ),
+                  ));
+            }),
+          );
+        });
   }
 }
